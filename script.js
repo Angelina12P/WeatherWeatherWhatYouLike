c264ea39f2b12f853b859a020d664b191ec5e290
Angel <rachel@Rachels-MacBook-Air.local>
2023-03-10 23:36:14 +0000

    got it showing on page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let APIKey = "5f06fd7d9130dd36b781c557ed9464e4";
+// let APIKey = "5f06fd7d9130dd36b781c557ed9464e4";
 // let searchCity = document.getElementById("searchCity");
 let searchCity = "london";
 let searchButton = document.getElementById("searchButton");
 
 // eventlistener event.target 
 
-// let searchWeather = () => {
+let searchWeather = () => {
 
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${APIKey}`)
     .then(response => response.json())
@@ -20,14 +20,25 @@ let searchButton = document.getElementById("searchButton");
 })
 
 .then(response => response.json())
-.then(data =>{
-    console.log()
-    result.innerHTML=`
-    <div>something</div>
+.then(weatherData =>{
+    console.log(weatherData)
+    let cityName = weatherData.name
+    let currentTemp = weatherData.main.temp
+    // let iconRep = ??? current.weather.icon
+    let humidity = weatherData.main.humidity
+    let windSpeed = weatherData.wind.speed
+
+    document.getElementById("answersBit").innerHTML =
+    `
+    <p>"City" ${cityName}</p>
+    <p>"Temp" ${currentTemp}</p>
+    // <p>not sure </p>
+    <p>"Humidity" ${humidity}</p>
+    <p>"Wind Speed" ${windSpeed}</p>
     `
     
 })
-// };
+};
 
-
+searchWeather()
 // searchButton.addEventListener(onclick, searchWeather);
